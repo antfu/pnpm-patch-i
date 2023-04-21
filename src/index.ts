89747ac52adbeb050b9f30c842dfaabde982f368
Anthony Fu <anthonyfu117@hotmail.com>
2023-04-21 11:24:49 +0200

    fix: cli

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 import { customAlphabet } from 'nanoid/non-secure'
 import { execa } from 'execa'
 import prompts from 'prompts'
+
 // @ts-expect-error missing types
 import launch from 'launch-editor'
 import c from 'picocolors'
@@ -11,7 +12,7 @@ const nanoid = customAlphabet('1234567890abcdef', 10)
 export async function startPatch(name: string, options: string[]) {
   const dir = `node_modules/.patch-edits/patch_edit_${name.replace(/\//g, '+')}_${nanoid()}`
 
-  await execa('pnpm', ['patch', ...options, '--edit-dir', dir, name], { stdout: 'ignore', stderr: 'inherit' })
+  await execa('pnpm', ['patch', ...options, '--edit-dir', dir, name], { stdio: 'inherit' })
   await launch(dir)
 
   console.log(`Edit your patch for ${c.bold(c.yellow(name))} under ${c.green(dir)}\n`)
