62891a2cfffd662e234b99dffc20c450c87a205c
Anthony Fu <github@antfu.me>
2025-10-31 14:52:14 +0900

    fix: cli options

--- src/cli.ts
+++ src/cli.ts
@@ -7,12 +7,14 @@ cli
   .command('<package-name> [source-dir]', 'Patch a package')
   .option('-y, --yes', 'Skip confirmation')
   .option('-b, --build', 'Build the source package before patching, only available when dir is not specified')
+  .option('-p, --pack', 'Pack the source package into a tgz file and patch the package from the tgz file, only available when dir is not specified')
   .action((name, sourceDir, options) => {
     return startPatch({
       name,
       yes: options.yes,
       sourceDir,
       build: options.build,
+      pack: options.pack,
     })
   })
 
